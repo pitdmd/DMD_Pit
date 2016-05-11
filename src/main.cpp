c5a4a06dbd2ad610292b90d49a284f19ceb0279a
Helmut Siedl <coin@siedl.priv.at>
2016-05-12 01:02:52 +0200

    adapt block old wallets triggertimer

--- src/main.cpp
+++ src/main.cpp
@@ -3277,9 +3277,9 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             return true;
         }
 
-        /* Disconnect all obsolete clients after 10 May 2016 12:00:00 UTC */
+        /* Disconnect all obsolete clients after 15 May 2016 12:00:00 UTC */
         uint nAdjTime = GetAdjustedTime();
-        if(nAdjTime > 1462881600) {
+        if(nAdjTime > 1463313600) {
             if(pfrom->nVersion < MIN_PROTOCOL_VERSION) {
                 printf("obsolete node %s with client %d, disconnecting\n",
                   pfrom->addr.ToString().c_str(), pfrom->nVersion);
